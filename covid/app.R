c22cad2ac5ba6a8e53a0073bcd7ba37818cccd8b
Fernando Greve <fgreve@gmail.com>
2020-06-09 18:56:22 -0400

    covid

---
+++ covid/app.R
@@ -0,0 +1,94 @@
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Covid-19 Chile" ),
+    #sidebar <- dashboardSidebar(sidebarMenu(), disable = TRUE),
+    
+    sidebar <- dashboardSidebar(collapsed = TRUE,
+        sidebarMenu (
+            menuItem("Fernando Greve", startExpanded = TRUE, menuSubItem(text = "fernandogreve.com", href = "https://fernandogreve.com/")))
+            
+        ),
+  
+    
+    
+    dashboardBody(
+        
+        h2("Total Nacional"),
+        
+        h5('Ultima actualizacion'),
+        textOutput("b"),
+        
+        fluidRow(valueBoxOutput("value1"),
+                 valueBoxOutput("value2"),
+                 valueBoxOutput("value3")),
+
+
+        checkboxGroupInput("var_nacional","Elegir:", c("Casos totales","Casos nuevos totales"), selected=c("Casos totales","Casos nuevos totales"), inline = TRUE),
+
+        dygraphOutput("dygraph_nacional"),
+                          
+        h2("Tasa de Contagios RM"),         
+        
+        plotlyOutput("plot"),
+        
+        leafletOutput("mymap"),
+        
+        dygraphOutput("dygraph"),
+        
+        selectInput("comuna","Elegir comunas",minsal$Comuna,selected=c("Nunoa","Independencia"),multiple=TRUE),
+   
+        
+        tags$footer(a(href="http://www.fernandogreve.com", target="_blank", "www.fernandogreve.com"))
+    
+    )
+
+)
+                 
+
+server = function(input, output, session){
+    output$mymap <- renderLeaflet({m})
+    
+    output$plot <- renderPlotly({p})
+    
+    output$dygraph <- renderDygraph({dygraph(time_series[,input$comuna], main = "Total Contagiados por comuna") %>% 
+            dyRangeSelector() %>% dyOptions(stackedGraph = TRUE) %>% dyRangeSelector(height = 20) %>%
+            dyHighlight(highlightSeriesOpts = list(strokeWidth = 3))
+        
+        })
+    
+    output$dygraph_nacional <- renderDygraph({dygraph(TotalesNacionales[,input$var_nacional], main = "Casos totales y Casos nuevos totales") %>% 
+            dyRangeSelector() %>% dyOptions(stackedGraph = TRUE) %>% dyRangeSelector(height = 20) %>%
+            dyHighlight(highlightSeriesOpts = list(strokeWidth = 3))
+        })
+    
+    
+    output$value1 <- renderValueBox({
+        valueBox(
+            formatC(last(TotalesNacionales$"Casos nuevos totales", 1), format="d", big.mark='.'),
+            names(TotalesNacionales$"Casos nuevos totales"),
+            color = "purple")   
+    })
+    
+    output$value2 <- renderValueBox({
+        valueBox(
+            formatC(last(TotalesNacionales$"Casos totales", 1), format="d", big.mark='.'),
+            names(TotalesNacionales$"Casos totales"),
+            color = "green")
+    })
+    
+    output$value3 <- renderValueBox({
+        valueBox(
+            formatC(last(TotalesNacionales$"Fallecidos", 1), format="d", big.mark='.'),
+            names(TotalesNacionales$"Fallecidos"),
+            color = "yellow")
+    })
+    
+    output$b <- renderText({time(last(TotalesNacionales))  %>% as.character() })
+
+    
+
+}
+
+
+shinyApp(ui, server)      
+      
---
+++ covid/global.R
@@ -0,0 +1,85 @@
+library(rgdal)
+library(dplyr)
+library(leaflet)
+library(ggplot2)
+library(plotly)
+library(tidyr)
+library(dygraphs)
+library(shiny)
+library(shinyWidgets)
+library(shinythemes) 
+library(shinydashboard)
+
+token = "pk.eyJ1IjoiZmdyZXZlIiwiYSI6ImNrYXpmMmI3dzA4Y3Yyc3AweW95eTR5azkifQ.Bou947cDslixneZpDKrImA"
+stgo <- readOGR("https://raw.githubusercontent.com/fgreve/d3-comunas-cl/master/data/comunas.json")
+class(stgo)
+
+minsal = read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv", 
+                  check.names = FALSE,
+                  encoding="UTF-8") %>% 
+    rename(cod_reg="Codigo region",id="Codigo comuna") %>%
+    filter(Region == "Metropolitana")
+
+stgo = sp::merge(stgo, minsal, by = "id")
+stgo <- stgo[!is.na(stgo@data$Region),]
+stgo <- stgo[stgo@data$Region=="Metropolitana",]
+
+no_stgo = c("Tiltil","Lampa","Colina","San Jose de Maipo","Pirque","Paine","Buin","Alhue","Melipilla","San Pedro","Maria Pinto","Curacavi","Penaflor","Calera de Tango","Padre Hurtado","El Monte","Talagante","Isla de Maipo","Lo Barnechea")
+
+stgo = stgo[!(stgo@data$Comuna %in% no_stgo),]
+
+bins <- c(400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, Inf)
+pal <- colorBin("YlOrRd", domain = stgo$Tasa, bins = bins)
+
+labels <- sprintf("<strong>%s</strong><br/>%g infected / population", stgo$Comuna, stgo$Tasa) %>% lapply(htmltools::HTML)
+
+m <- leaflet(stgo) %>%
+    setView(lng=-70.64724, lat=-33.47269, zoom=11) %>%
+    addProviderTiles("MapBox", options=providerTileOptions(id="mapbox.light", accessToken=token)) %>% 
+    addPolygons(
+        fillColor = ~pal(Tasa),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(weight = 5, color = "#666", dashArray = "", fillOpacity = 0.7, bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(style = list("font-weight"="normal", padding="3px 8px"), textsize="15px", direction = "auto")) %>% 
+    addLegend(pal = pal, values = ~Tasa, opacity = 0.7, title = NULL, position = "bottomright")
+
+# series de tiempo por comuna (Numero de contagios)
+library(xts)
+time_series = minsal %>% 
+    select(-Region,-cod_reg,-id,-Poblacion,-Tasa) %>% 
+    gather("Fecha","Contagiados",-Comuna) %>% 
+    spread(Comuna,Contagiados)   
+    
+time_series$Fecha = as.Date(time_series$Fecha)
+time_series <- xts(time_series[,-1], order.by=as.Date(time_series[,1], "%Y-%m-%d"))
+str(time_series)
+
+# barra de tasas de contagio por comuna
+df = minsal %>% select(Comuna,Tasa)
+df$Comuna <- factor(df$Comuna, levels = unique(df$Comuna)[order(df$Tasa, decreasing = FALSE)])
+p = df %>% plot_ly(x = ~Comuna, y = ~Tasa, type = "bar", name = 'Contagios')
+
+# TotalesNacionales
+producto5 = read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales.csv", 
+                           check.names = FALSE, encoding="UTF-8")
+
+TotalesNacionales = producto5 %>% gather("Date","Valor",-Fecha) %>% spread(Fecha,Valor) %>% rename(Fecha = Date)
+TotalesNacionales$Fecha = as.Date(TotalesNacionales$Fecha)
+TotalesNacionales <- xts(TotalesNacionales[,-1], order.by=as.Date(TotalesNacionales[,1], "%Y-%m-%d"))
+str(TotalesNacionales)
+
+# CasosRegionales
+producto3 = read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/CasosTotalesCumulativo.csv", 
+                     check.names = FALSE, encoding="UTF-8")
+
+CasosRegionales = producto3 %>% gather("Date","Valor",-Region) %>% spread(Region,Valor) %>% rename(Fecha = Date)
+CasosRegionales$Fecha = as.Date(CasosRegionales$Fecha)
+CasosRegionales <- xts(CasosRegionales[,-1], order.by=as.Date(CasosRegionales[,1], "%Y-%m-%d"))
+str(CasosRegionales)
+
+rm(stgo, CasosRegionales, df, labels, producto3, producto5)
